1b4c0729d232d88a96f1ac9f58640df81b582a82
maxnth <maximilian.noeth@protonmail.com>
2024-07-30 13:51:45 +0200

    fix: add null pointer checks for relations

--- java/PrimaDla/src/org/primaresearch/dla/page/io/xml/XmlPageWriter_2016_07_15.java
+++ java/PrimaDla/src/org/primaresearch/dla/page/io/xml/XmlPageWriter_2016_07_15.java
@@ -627,10 +627,10 @@ public class XmlPageWriter_2016_07_15 implements XmlPageWriter {
 				//Type
 				addAttribute(relationNode, DefaultXmlNames.ATTR_type, rel.getRelationType().toString());
 				//Custom
-				if (!rel.getCustomField().isEmpty())
+				if (rel.getCustomField() != null && !rel.getCustomField().isEmpty())
 					addAttribute(relationNode, DefaultXmlNames.ATTR_custom, rel.getCustomField());
 				//Comments
-				if (!rel.getComments().isEmpty())
+				if (rel.getComments() != null && !rel.getComments().isEmpty())
 					addAttribute(relationNode, DefaultXmlNames.ATTR_comments, rel.getComments());
 				
 				//Object 1
